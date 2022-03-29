2fe539b9d9a9abc54487c13f0effbb031647887e
maxt0d0nt <maxbox21@hotmail.com>
2022-03-29 10:39:17 -0300

    falta todo el node module

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import freeCampCodeLogo from './img/banner.png';
+import Tarea from './components/Tarea.jsx';
+
+function App() {
+  return (
+    <div className="App">
+     <div className='freecodecamp-logo-contenedor'>
+       <img src={freeCampCodeLogo} className='frecodecamp-logo'/>
+     </div>
+     <div className='contenedor-tareas'>
+       <div className='tarea-lista-principal'>
+       <h1>Mis Tareas</h1>
+       <Tarea />
+       </div>
+     </div>
+    </div>
+  )
+};
+
+export default App;
